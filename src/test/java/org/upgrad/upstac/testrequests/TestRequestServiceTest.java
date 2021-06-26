98941ed062cf3d60570f58bd8c80b141b9b81655
Prakhar Saxena <prakhar@gmail.com>
2021-06-27 02:54:45 +0530

    second commit

---
+++ src/test/java/org/upgrad/upstac/testrequests/TestRequestServiceTest.java
@@ -0,0 +1,16 @@
+package org.upgrad.upstac.testrequests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TestRequestServiceTest {
+
+    @Test
+    public void test(){
+        System.out.println("Test...");
+    }
+
+
+}
